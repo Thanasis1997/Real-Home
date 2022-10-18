2427db3cf5fc83dcd2c6d181d9c898c7b781ac1b
Thanasis <thanasis1997abc@gmail.com>
2022-10-18 19:53:53 +0300

    thanasis change

--- app/src/main/java/com/example/myrealestate/MainActivity.kt
+++ app/src/main/java/com/example/myrealestate/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("31")
+        print("test1221")
 
 
     }
